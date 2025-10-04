6516a12bb14f2c9311f90b596443d960d1711b8c
zainaliakbar <zaynaliakbar@gmail.com>
2025-10-04 22:03:05 +0500

    commit

---
+++ Javascript Variables/script.js
@@ -0,0 +1,18 @@
+console.log("Hey");
+
+var a=2;
+var b=2;
+console.log(a+b);
+
+var name="John";
+var age=30;
+console.log("My name is " + name + " and I am " + age + " years old.");
+
+console.log(typeof a, typeof b, typeof name);
+
+// variables are case-sensitive name and Name are different
+
+// Var has GLOBAL scope but Let and Const have BLOCK scope
+// Const cannot be reassigned and is used to keep same value permanently
+
+
