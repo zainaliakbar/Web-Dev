0e68b01f8d374b4f932ae0b1a3f7cc6249bb77fa
zainaliakbar <zaynaliakbar@gmail.com>
2025-10-04 20:44:38 +0500

    commit

--- Javascript Basic/script.js
+++ Javascript Basic/script.js
@@ -1,4 +1,21 @@
-alert("Hello world")
+alert("Hey");
 
 // Displaying message in console in inspect element on sites
-console.log("Hello world")
+    
+var number = prompt("Enter your number")
+    
+var istrue = confirm("Are you sure you want to enter this number?")
+
+if(istrue){
+    console.log("Your number is entered successfully")
+}
+else{
+    console.log("Number not entered")
+}
+console.log("Your number is" + number)
+
+// JS can be used to do css tasks and html like bg color and doc title
+
+// document.body.style.backgroundColor = "lightblue"
+
+// document.title = "I am a new title"
