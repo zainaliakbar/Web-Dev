c70bc66627968b0bc1d9daa505109182ac28c1d4
zainaliakbar <zaynaliakbar@gmail.com>
2025-10-04 22:40:34 +0500

    commit

--- Javascript Variables/script.js
+++ Javascript Variables/script.js
@@ -11,8 +11,33 @@ console.log("My name is " + name + " and I am " + age + " years old.");
 console.log(typeof a, typeof b, typeof name);
 
 // variables are case-sensitive name and Name are different
-
 // Var has GLOBAL scope but Let and Const have BLOCK scope
 // Const cannot be reassigned and is used to keep same value permanently
+
+let Name="Zain";
+let Age=22;
+let height=5.11;
+let u=undefined;
+const p= true;
+let r=null;
+
+console.log(typeof Name, typeof age, typeof height, typeof u, typeof p, typeof r);
+console.log( Name, Age,  height, u, p, r);
+
+// object has a key and value pair, null is also an object in javascript
+
+let o={
+    firstName: "Zain",
+    job: "Web Developer",
+    "degree time": 4.5
+}
+
+console.log(o);
+
+o.income="10 lakh";
+
+console.log(o);
+
+
 
 
