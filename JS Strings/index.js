dce33c03ab94f83ce4e9d5d495dacc78ff0cd54b
zainaliakbar <zaynaliakbar@gmail.com>
2025-10-05 22:09:30 +0500

    commit

---
+++ JS Strings/index.js
@@ -0,0 +1,18 @@
+let a="Zain";
+
+console.log(a);
+console.log(a[2]);
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.slice(1,4));
+console.log(a.replace("Z","J"));    
+console.log(a.concat(" Ali"));
+console.log(a.trim());  //remove white spaces or extra spaces
+console.log(a.charAt(2));
+
+console.log("my name is",a);
+
+// /template literals
+console.log(`my name is ${a}`);
+
