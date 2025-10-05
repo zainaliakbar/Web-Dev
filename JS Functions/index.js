e626315e70527aedd5bbb8ce1f5a7b31860e0986
zainaliakbar <zaynaliakbar@gmail.com>
2025-10-05 19:33:55 +0500

    commit

---
+++ JS Functions/index.js
@@ -0,0 +1,13 @@
+function intro(name) {
+    console.log("hi "+ name);
+    console.log(name + "plays");
+    console.log(name + "sleeps");
+}
+
+intro("zain");
+
+
+
+
+
+
---
+++ JS Loops/index.js
@@ -0,0 +1,23 @@
+console.log("JS Loops");
+let a=0;
+console.log(a);
+
+for(let i=0; i<5; i++){
+    console.log(a+=2);
+}
+
+let o={
+    name:"Zain",
+    degree:"Data Science",
+    interest:"Web Development"
+}
+
+for (const key in o){
+    console.log(o[key]);
+}
+
+let i=0;
+while(i<5){
+    console.log(i);
+    i++;
+}
